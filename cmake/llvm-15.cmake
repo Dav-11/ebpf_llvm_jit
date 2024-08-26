b27d5efc390b4f0f2e0a4f06d2c7a16524f0b010
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-08-26 19:44:41 +0000

    devcontainer

--- cmake/llvm-15.cmake
+++ cmake/llvm-15.cmake
@@ -1,42 +1,58 @@
 # llvm-15.cmake
 
-include(FetchContent)
+option(ENABLE_LLVM_SHARED "Link shared library of LLVM" YES)
 
-message(STATUS "Downloading and building llvm...")
+if(ENABLE_LLVM_SHARED)
 
-FetchContent_Declare(
-        llvm
-        GIT_REPOSITORY https://github.com/llvm/llvm-project.git
-        GIT_TAG llvmorg-15.0.0
-        GIT_SHALLOW TRUE
-)
+        message(STATUS "Configuring project for shared LLVM...")
 
-# Set LLVM build options
-set(LLVM_ENABLE_PROJECTS "clang;lld;libcxx;libcxxabi" CACHE STRING "LLVM projects to build")
-set(LLVM_TARGETS_TO_BUILD "X86" CACHE STRING "Targets to build")
-set(LLVM_ENABLE_RTTI ON CACHE BOOL "Enable RTTI")
-set(LLVM_ENABLE_EH ON CACHE BOOL "Enable Exception Handling")
+        find_package(LLVM REQUIRED CONFIG)
 
-# Download and build LLVM
-FetchContent_MakeAvailable(llvm)
+        if (${LLVM_PACKAGE_VERSION} VERSION_LESS 15)
+                message(FATAL_ERROR "LLVM version must be >=15")
+        endif()
 
-# Set LLVM CMake configuration
-set(LLVM_DIR ${llvm_SOURCE_DIR}/llvm/cmake/modules CACHE PATH "Path to LLVM cmake modules")
+        set(LLVM_LIBRARIES LLVM)
+else()
 
-# Locate LLVM package
-find_package(LLVM REQUIRED CONFIG)
+        include(FetchContent)
 
-# Link LLVM components
-llvm_map_components_to_libnames(LLVM_LIBRARIES
-        Core
-        OrcJIT
-        mcjit
-        Support
-        nativecodegen
-)
+        message(STATUS "Downloading and building llvm...")
 
-# Provide LLVM variables to the main CMakeLists.txt
-set(LLVM_INCLUDE_DIRS ${LLVM_INCLUDE_DIRS})
-set(LLVM_LIBRARIES ${LLVM_LIBRARIES})
+        FetchContent_Declare(
+                llvm
+                GIT_REPOSITORY https://github.com/llvm/llvm-project.git
+                GIT_TAG llvmorg-15.0.0
+                GIT_SHALLOW TRUE
+        )
+
+        # Set LLVM build options
+        set(LLVM_ENABLE_PROJECTS "clang;lld;libcxx;libcxxabi" CACHE STRING "LLVM projects to build")
+        set(LLVM_TARGETS_TO_BUILD "X86" CACHE STRING "Targets to build")
+        set(LLVM_ENABLE_RTTI ON CACHE BOOL "Enable RTTI")
+        set(LLVM_ENABLE_EH ON CACHE BOOL "Enable Exception Handling")
+
+        # Download and build LLVM
+        FetchContent_MakeAvailable(llvm)
+
+        # Set LLVM CMake configuration
+        set(LLVM_DIR ${llvm_SOURCE_DIR}/llvm/cmake/modules CACHE PATH "Path to LLVM cmake modules")
+
+        # Locate LLVM package
+        find_package(LLVM REQUIRED CONFIG)
+
+        # Link LLVM components
+        llvm_map_components_to_libnames(LLVM_LIBRARIES
+                Core
+                OrcJIT
+                mcjit
+                Support
+                nativecodegen
+        )
+
+        # # Provide LLVM variables to the main CMakeLists.txt
+        # set(LLVM_INCLUDE_DIRS ${LLVM_INCLUDE_DIRS})
+        # set(LLVM_LIBRARIES ${LLVM_LIBRARIES})
+endif()
 
 message(STATUS "DONE")
