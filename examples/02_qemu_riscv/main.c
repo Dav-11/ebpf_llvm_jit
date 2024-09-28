05d99b32aad48862c6f09c1ebf87d37ff36b1453
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-09-28 15:31:24 -0400

    minor fixes

--- examples/02_qemu_riscv/main.c
+++ examples/02_qemu_riscv/main.c
@@ -22,6 +22,8 @@ extern uint32_t my_data_region_end;
 
 #define E_END_REGION -3
 
+#define BUF_SIZE 50
+
 int load_packet_from_mem(void *region_start, void *region_end, struct xdp_md *packet)
 {
     packet->data = (__u32)region_start;
@@ -31,6 +33,8 @@ int load_packet_from_mem(void *region_start, void *region_end, struct xdp_md *pa
     __u32 pkt_end = 0;
 
     int count = 0;
+
+    uart_puts("++++ Packet content ++++\n");
 
     // Scan the memory for a 64-bit sequence of all 1s
     while (*curr != 0xFFFFFFFF || *prev != 0xFFFFFFFF) {
@@ -46,7 +50,7 @@ int load_packet_from_mem(void *region_start, void *region_end, struct xdp_md *pa
         }
 
         char str[20] = "[0x%8x]: 0x%8x\n";
-        _bpf_helper_ext_0006(str,20 ,curr, *curr);
+        _bpf_helper_ext_0006(str, 20, curr, *curr);
 
       prev = curr;
         curr++;
@@ -54,41 +58,53 @@ int load_packet_from_mem(void *region_start, void *region_end, struct xdp_md *pa
     }
 
     // Set pkt_end to the address right before the 64-bit sequence of 1s
-    pkt_end = (__u32)prev;
+    packet->data_end = (__u32)prev;
 
-    packet->data_end = (__u64)pkt_end;
+    // set values to default
+    packet->ingress_ifindex = 99;
+    packet->rx_queue_index = 0;
+    packet->egress_ifindex = 0;
+    packet->data_meta = 0;
+
+    uart_puts("\n++++ END +++++\n\n");
 }
 
 
 void main() {
     UART0_FCR = UARTFCR_FFENA;    // Set the FIFO for polled operation
-    uart_puts("Hello World!\n");  // Write the string to the UART
+    uart_puts("Started runtime\n");  // Write the string to the UART
+
+    char str[BUF_SIZE] = "initialized";
 
     // test bpf_printk();
 
-    char str[20] = "Hello: %d\n";
-    _bpf_helper_ext_0006(str,20 ,6);
-
-    strcpy(str, "Hello: %d\n");
-    _bpf_helper_ext_0006(str,20 ,9);
+//    strcpy(str, "Hello: %d\n");
+//    _bpf_helper_ext_0006(str, BUF_SIZE, 6);
+//
+//    strcpy(str, "Hello: %d\n");
+//    _bpf_helper_ext_0006(str, BUF_SIZE, 9);
 
     // print region location
-    strcpy(str, "Start Region: 0x%x\n");
-    _bpf_helper_ext_0006(str,20 ,&my_data_region_start);
+    strcpy(str, "Start MM Region: 0x%x \n");
+    _bpf_helper_ext_0006(str, BUF_SIZE, &my_data_region_start);
 
-    strcpy(str, "End Region: 0x%x\n");
-    _bpf_helper_ext_0006(str,20 ,&my_data_region_end);
+    strcpy(str, "End MM Region: 0x%x \n");
+    _bpf_helper_ext_0006(str, BUF_SIZE, &my_data_region_end);
 
     // test load pkt from mem
     struct xdp_md packet;
     load_packet_from_mem(&my_data_region_start, &my_data_region_end, &packet);
 
     strcpy(str, "Start Pkt: 0x%x\n");
-   _bpf_helper_ext_0006(str,20 ,packet.data);
+   _bpf_helper_ext_0006(str, BUF_SIZE, packet.data);
 
     strcpy(str, "End Pkt: 0x%x\n");
-    _bpf_helper_ext_0006(str,20 ,packet.data_end);
+    _bpf_helper_ext_0006(str, BUF_SIZE, packet.data_end);
 
+    int ret = bpf_main(&packet, sizeof(packet));
+
+    strcpy(str, "XDP result: %d\n");
+    _bpf_helper_ext_0006(str, BUF_SIZE, ret);
 
     while (1); // Loop forever to prevent program from ending
 }
