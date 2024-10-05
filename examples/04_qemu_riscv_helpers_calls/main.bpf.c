6b5395783ad863cf7ac4bbeb98236f4869229e94
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-10-05 17:58:50 +0000

    updated linker script memory load

--- examples/04_qemu_riscv_helpers_calls/main.bpf.c
+++ examples/04_qemu_riscv_helpers_calls/main.bpf.c
@@ -5,12 +5,17 @@
 #include <linux/ip.h>
 #include <bpf/bpf_endian.h>
 #include <stdint.h>
+#include <string.h>
 
 SEC("xdp")
 int hello_world(struct xdp_md *ctx) {
 
-    bpf_printk("Hello World from XDP\0");
-    return 5;
+    // char buf[30];
+    // strcpy(buf, "Hello World from XDP\n\0");
+
+    bpf_printk("Hello World from XDP\n\0");
+
+    return XDP_PASS;
 }
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
